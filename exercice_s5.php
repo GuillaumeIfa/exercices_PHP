0b6a91ec71831518485f9278ce6f3331f36c7818
GuillaumeIfa <ory.guillaume@stagiairesifa.fr>
2019-01-30 14:59:58 +0100

    commit exercice_s5 ; partie III

--- exercice_s5.php
+++ exercice_s5.php
@@ -99,5 +99,61 @@
 
 		</form>
 
+		<br><br><hr><br><br>
+		<h3> Exercice I.III </h3><br>
+
+		<?php 
+
+			$nom3 = "";
+			$prenom3 = "";
+
+		?>
+
+		<form name="form3" method="POST" action="exercice_s5.php">
+
+			Prenom:<br><input type="text" value="<?php echo $prenom3; ?>" name="prenom3"><br>
+			Nom:<br><input type="text" value="<?php echo $nom3; ?>" name="nom3"><br>
+			<input type="submit" name="submit3"><br><br>
+
+		</form>		
+
+
+		<?php  
+
+			if (isset($_POST['submit3'])) 
+			{
+				$prenom = $_POST['prenom3'];
+				$nom = $_POST['nom3'];
+			}
+
+			$prenom3 = strip_tags($prenom);
+			$nom3 = strip_tags($nom);
+			$test = 0;
+			
+			$users = array
+			(
+				"johnny hallyday", 
+				"simon bertrand", 
+				"tom hanks", 
+				"toto tata", 
+				"john john"
+			);
+
+			foreach ($users as  $value) 
+			{
+				if ($value == $prenom.' '.$nom) 
+				{
+					$test = 1;
+				}
+			}
+
+			$prenom3 = ucfirst($prenom3);
+			$nom3 = ucfirst($nom3);
+
+			if ($test == 1) 
+			{
+				echo 'Bienvenue ' .$prenom3. ' ' .$nom3. ' !';
+			}
+		?>
 	</body>
 </html>
